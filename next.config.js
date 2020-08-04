701a4f32b161bf7b18240ba8bf82ea394bda69af
関根慧 <sekine@freedomland.jp>
2020-08-04 17:39:27 +0900

    Github pages用に修正
    Github pages用
    
    Github pages用
    
    修正

--- next.config.js
+++ next.config.js
@@ -1,7 +1,28 @@
 const path = require('path')
 
 module.exports = {
+    assetPrefix: '/next_app',
     sassOptions: {
         includePaths: [path.join(__dirname, 'styles')],
+    },
+    exportTrailingSlash: true,
+    exportPathMap: async function() {
+        const paths = {
+            '/': { page: '/' },
+            // '/chapter': { page: '/chapter' },
+        };
+        // const paths = {};
+        // paths[`/`] = { page: '/'};
+        // paths[`/chapter`] = { page: '/chapter'};
+        // const chapters = ["first", "second", "third"];
+        // chapters.forEach(item => {
+        //     paths[`/chapter/${item}`] = { page: '/chapter'};
+        // });
+
+        return paths;
+    },
+    publicRuntimeConfig: {
+        // Will be available on both server and client
+        basePath: '/next_app',
     },
 }
